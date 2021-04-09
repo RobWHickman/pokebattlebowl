4be23c1c61e8f7f976ca7ab9bdf3db3c68104813
rob-fd <robert@focaldata.com>
2021-04-10 00:18:25 +0100

    initial tests

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pokebattlebowl
 A twitter bot for simulating pokemon battles; mostly and excuse to learn new tech
+
+
+## Inspirations
+
+[Matt Dray's post on creating a twitter bot with Github Actions and {rtweet}](https://www.rostrum.blog/2020/09/21/londonmapbot/)
+
---
+++ testpost.R
@@ -0,0 +1,14 @@
+twitter_token <- rtweet::create_token(
+  app = "londonmapbot",
+  consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
+
+tweettext <- paste0("I am a twitter bot test. The time is currently: ", Sys.time())
+
+rtweet::post_tweet(
+  status = tweettext,
+  token = twitter_token
+)
